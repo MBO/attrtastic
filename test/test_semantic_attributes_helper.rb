355ebc3b488c4528d90fd147b3807047ec65784f
MBO <boruta.miroslaw+git@gmail.com>
2010-01-02 18:27:29 +0100

    reorganized tests

--- test/test_semantic_attributes_helper.rb
+++ test/test_semantic_attributes_helper.rb
@@ -6,7 +6,7 @@ class SemanticAttributesHelper < Test::Unit::TestCase
     setup_fixtures
   end
 
-  def test_empty_semantic_attributes_for
+  def test_empty__semantic_attributes_for
     expected = html <<-EOHTML
       <div class="attrtastic user">
       </div>
@@ -15,9 +15,19 @@ class SemanticAttributesHelper < Test::Unit::TestCase
     @template.semantic_attributes_for(@user)
     actual = @template.output_buffer
     assert_equal expected, actual
+
+    @template.output_buffer.clear
+
+    expected = html <<-EOHTML
+      <div class="attrtastic blog">
+      </div>
+    EOHTML
+    @template.semantic_attributes_for(@blog)
+    actual = @template.output_buffer
+    assert_equal expected, actual
   end
 
-  def test_semantic_attributes_for
+  def test__semantic_attributes_for__should_run_block
     expected = html <<-EOHTML
       <div class="attrtastic user">
       </div>
@@ -27,10 +37,8 @@ class SemanticAttributesHelper < Test::Unit::TestCase
     @template.semantic_attributes_for(@user) do
       block_run = true
     end
-    actual = @template.output_buffer
 
     assert block_run
-    assert_equal expected, actual
   end
 
 end
