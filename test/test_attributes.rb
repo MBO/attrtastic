545e4ff25369384d9745598a8ecafa2a7cce2509
Corin Langosch <corin.langosch@netskin.com>
2011-06-21 11:29:13 +0200

    fixed failing tests

--- test/test_attributes.rb
+++ test/test_attributes.rb
@@ -42,7 +42,7 @@ class TestAttributes < TestCase
     should "show header" do
       expected = html <<-EOHTML
         <div class="attributes">
-          <div class="legend">Legend</div>
+          <div class="legend"><span>Legend</span></div>
           <ol>
           </ol>
         </div>
@@ -82,7 +82,7 @@ class TestAttributes < TestCase
       should "show header" do
         expected = html <<-EOHTML
           <div class="attributes contact">
-            <div class="legend">Contact</div>
+            <div class="legend"><span>Contact</span></div>
             <ol>
               <li class="attribute">
                 <span class="label">Full name</span>
@@ -156,7 +156,7 @@ class TestAttributes < TestCase
       should "show header" do
         expected = html <<-EOHTML
           <div class="attributes user">
-            <div class="legend">Author</div>
+            <div class="legend"><span>Author</span></div>
             <ol>
             </ol>
           </div>
@@ -225,7 +225,7 @@ class TestAttributes < TestCase
 
         expected = html <<-EOHTML
           <div class="attributes user">
-            <div class="legend">Author</div>
+            <div class="legend"><span>Author</span></div>
             <ol>
             </ol>
           </div>
@@ -285,12 +285,12 @@ class TestAttributes < TestCase
       should "show header" do
         expected = html <<-EOHTML
           <div class="attributes post">
-            <div class="legend">Post</div>
+            <div class="legend"><span>Post</span></div>
             <ol>
             </ol>
           </div>
           <div class="attributes post">
-            <div class="legend">Post</div>
+            <div class="legend"><span>Post</span></div>
             <ol>
             </ol>
           </div>
@@ -360,12 +360,12 @@ class TestAttributes < TestCase
       should "show header" do
         expected = html <<-EOHTML
           <div class="attributes post">
-            <div class="legend">Post</div>
+            <div class="legend"><span>Post</span></div>
             <ol>
             </ol>
           </div>
           <div class="attributes post">
-            <div class="legend">Post</div>
+            <div class="legend"><span>Post</span></div>
             <ol>
             </ol>
           </div>
